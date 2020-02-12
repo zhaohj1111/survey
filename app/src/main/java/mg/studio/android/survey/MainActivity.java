f788627fca76365a67be0163899f292cf69861d8
Feilong <feilong.dragona@gmail.com>
2020-02-12 12:30:05 +0800

    Core project: survey

---
+++ app/src/main/java/mg/studio/android/survey/MainActivity.java
@@ -0,0 +1,14 @@
+package mg.studio.android.survey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weclome);
+    }
+}
